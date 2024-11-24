42813e85cb10ef399d64ca619b504ed118687f5a
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-11-24 21:20:30 +0300

    React №1

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -21,13 +21,25 @@ const TodoItem = ({ todo, setTodos }) => {
     <li key={todo.id} className={`${todo.completed ? "completed" : ""}`}>
       <h4>{todo.title}</h4>
       {!todo.completed && (
-        <button onClick={() => changeStatus(todo.id)}>Bajarish</button>
+        <button
+          onClick={() => changeStatus(todo.id)}
+          className={todo.completed ? "completed" : ""}
+        >
+          Do <i class="fa-solid fa-check"></i>
+        </button>
       )}
       {todo.completed && (
-        <button onClick={() => changeStatus(todo.id)}>Qaytarish</button>
+        <button
+          onClick={() => changeStatus(todo.id)}
+          className={todo.completed ? "completed" : ""}
+        >
+          Return <i class="fa-solid fa-rotate-left"></i>
+        </button>
       )}
 
-      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      <button onClick={() => deleteTodo(todo.id)} className="delete-btn">
+        <i class="fa-solid fa-trash"></i> Delete
+      </button>
     </li>
   );
 };
